96a7b646ae7361736aa6729c86af923a21762f98
Константин <danov@ancud.local>
2024-11-29 17:10:35 +0300

    models.py(add): add model UsersAchievementsOrm
    UsersAchievementsOrm используется для реализации связи многие-ко-многим для таблиц Users и Achievements

--- src/models.py
+++ src/models.py
@@ -1,13 +1,16 @@
 import enum
+import datetime
 from typing import Annotated
 
-from sqlalchemy import CheckConstraint, ForeignKey, String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import CheckConstraint, ForeignKey, String, text
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from database import Base
 
-intpk = Annotated[int, mapped_column(primary_key=True)]
+
 str_100 = Annotated[str, String(100)]
+intpk = Annotated[int, mapped_column(primary_key=True)]
+present_at = Annotated[datetime.datetime, mapped_column(server_default=text("now()"))]
 
 
 class LanguageOrm(enum.Enum):
@@ -22,6 +25,11 @@ class UsersOrm(Base):
     username: Mapped[str_100]
     language: Mapped[LanguageOrm]
 
+    received_achievements: Mapped[list["AchievementOrm"]] = relationship(
+        back_populates="received_users",
+        secondary="users_achievements",
+    )
+
 
 class AchievementOrm(Base):
     __tablename__ = "achievements"
@@ -33,6 +41,25 @@ class AchievementOrm(Base):
     description_en: Mapped[str]
     value: Mapped[int]
 
+    received_users: Mapped[list["UsersOrm"]] = relationship(
+        back_populates="received_achievements",
+        secondary="users_achievements",
+    )
+
     __table_args__ = (
         CheckConstraint("value > 0", name="achievement_value_positive"),
     )
+
+
+class UsersAchievementsOrm(Base):
+    __tablename__ = "users_achievements"
+
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey("users.id", ondelete="CASCADE"),
+        primary_key=True,
+    )
+    achievement_id: Mapped[int] = mapped_column(
+        ForeignKey("achievements,id", ondelete="CASCADE"),
+        primary_key = True,
+    )
+    present_at: Mapped[present_at]
