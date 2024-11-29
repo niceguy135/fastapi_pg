9358457b11e3d80255d4dc9253f1ac5906f3e3e5
Константин <danov@ancud.local>
2024-11-30 00:52:48 +0300

    src/migrations/env.py(mod): edit models metadata and DSN

--- src/migrations/env.py
+++ src/migrations/env.py
@@ -5,6 +5,10 @@ from sqlalchemy import pool
 
 from alembic import context
 
+from src.models import UsersOrm # noqa
+from src.database import Base
+from src.config import settings
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -18,13 +22,17 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
 
+config.set_main_option(
+    "sqlalchemy.url",
+    settings.DATABASE_URL_asyncpg + "?async_fallback=True"
+)
 
 def run_migrations_offline() -> None:
     """Run migrations in 'offline' mode.
