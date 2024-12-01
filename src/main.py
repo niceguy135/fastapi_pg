8f16e8a2ae6109f00a16d404c9de5e50ef4e2c17
Константин <danov@ancud.local>
2024-12-01 19:19:49 +0300

    main.py(mod): make first working fastapi service with stubs in routes

--- src/main.py
+++ src/main.py
@@ -2,10 +2,22 @@ import os
 import sys
 
 import uvicorn
+
 from fastapi import FastAPI, Body
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.exceptions import HTTPException
+
+from alembic.config import Config
+from alembic import command
 
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
+
+
+def run_migrations(alembic_cfg):
+    command.upgrade(alembic_cfg, "head")
+
+def downgrade_migration(alembic_cfg):
+    command.downgrade(alembic_cfg, "base")
 
 
 def create_fastapi_app():
@@ -48,13 +60,20 @@ def create_fastapi_app():
         pass
     ##################################################
 
+    @app.get("/", tags=["default"])
+    async def default_route():
+        return HTTPException(404)
+
     return app
 
 
 fastapi_app = create_fastapi_app()
 
 if __name__ == "__main__":
+    alembic_ini = Config("alembic.ini")
+    run_migrations(alembic_ini)
     uvicorn.run(
         app="src.main:fastapi_app",
         reload=True,
     )
+    downgrade_migration(alembic_ini)
